229aa47576b8cb215a3ab8bf1e9c2f776eb8873e
chrislemus <icrislemus@gmail.com>
2021-03-15 11:02:22 -0400

    commit tree 96028e45f24d679d68a33698fd064af3633867ac

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solutions in this file
+function writeCards(names) {
+  return names.map(name =>  `Thank you, ${name}, for the wonderful surprise gift!`);
+}
+
+function countDown(number) {
+  for (let index = 0; index <= number; index++) {
+    console.log(index)
+  }
+}
